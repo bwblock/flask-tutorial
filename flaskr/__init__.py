244b20e7ce5efd90513bbfe8a5c688b4e080ab65
B.W. Block <bbwilliam3@gmail.com>
2018-10-07 16:05:14 -0600

    dev

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -14,6 +14,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
